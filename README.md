b79aa5cca62efb6490c4fc91a5205557f7d62be1
cexperto <sipuedeshazloya@gmail.com>
2022-11-19 18:32:56 -0500

    readme

---
+++ README.md
@@ -0,0 +1,27 @@
+
+#  ACME
+
+System to calculate salary from txt file
+
+## Deployment
+
+To deploy this project run
+
+```bash
+  python main.py
+```
+
+To add data, modify employes.txt file or 
+change the name file in line 8 in main.py file
+
+the format data should be like below
+
+Example
+
+    RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00
+
+
+## Authors
+
+- [@cexperto](https://www.github.com/cexperto)
+
---
+++ validate.py
@@ -0,0 +1,18 @@
+class ValidateHour:
+
+    def validate_number(self, num):
+        num1 = num[0:2]
+        num2 = num[3:5]
+        if (not num1.isnumeric() or
+            not num2.isnumeric() or
+            num[2] != ':'):
+            return False
+        return True
+
+    def validate_hour(self, hour):
+        hour = str(hour)
+        if (len(hour) != 5 or
+            int(hour[0:2]) >= 24 or
+            int(hour[3:5]) > 59):
+            return False
+        return True
